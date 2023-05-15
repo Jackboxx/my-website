35bf9d867056fe9be232e64284abf22437d4aa8a
Jackboxx <gschwantnermoritz@gmail.com>
2023-05-15 13:54:20 +0200

    remove contractions from aboutMe

--- src/lib/sections/aboutMe.svelte
+++ src/lib/sections/aboutMe.svelte
@@ -9,14 +9,14 @@
 				class="max-w-[1200px] text-lg text-text-light
                 dark:text-text-dark lg:text-xl 2xl:text-3xl"
 			>
-				I'm a software developer currently based in Styria. I love working on software
+				I am a software developer currently based in Styria. I love working on software
 				related projects and tinkering with my development environment in my free time.
 				<br />
 				At the moment I am working at TimeTac (a time management software company) as a frontend
 				web developer using TypeScript, React, and Redux. Since I work fully remote at this
 				job my communication abilities are key to a successful business relationship.
 				<br />
-				Lastly, I've been enjoying using Rust
+				Lastly, I have been enjoying using Rust
 				<img class="inline h-7 2xl:h-9" alt="" src="/img/ferris-happy.svg" />
 				to write development tools (you can see some of those in the "Projects" section below).
 			</div>
